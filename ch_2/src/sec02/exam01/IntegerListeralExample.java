a6f6f79360481f851d83d68e37cd9f9c1572f07e
Luncho8 <wkdwlsgh8@naver.com>
2023-05-02 22:03:45 +0900

    secoind commit

---
+++ ch_2/src/sec02/exam01/IntegerListeralExample.java
@@ -0,0 +1,18 @@
+package sec02.exam01;
+
+public class IntegerListeralExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int var1 = 0b1011;
+		long var2 = 0206L;
+		float var3 = 365f;
+		double var4 = 0xB3;
+		
+		System.out.println("var1:" + var1);
+		System.out.println("var2:" + var2);
+		System.out.println("var3:" + var3);
+		System.out.println("var4:" + var4);
+	}
+
+}
---
+++ ch_2/src/sec03/exam01/CastingExample.java
@@ -0,0 +1,24 @@
+package sec03.exam01;
+
+public class CastingExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int intValue = 44032;
+		char charValue = (char) intValue;
+		System.out.println(charValue);
+		
+		String str = "1234";
+		int num = Integer.parseInt(str);
+		System.out.println(num);
+		
+		long longValue = 500;
+		intValue = (int) longValue;
+		System.out.println(intValue);
+		
+		double doubleValue = 3.14;
+		intValue = (int)doubleValue;
+		System.out.println(intValue);
+	}
+
+}
